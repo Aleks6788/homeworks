969bc9b41082dc95a96bbc79094dfc67cab11e9c
leshchev alekcei <luolikl@rambler.ru>
2023-02-16 15:00:50 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Task 2
+
+Console.Write("Введите первое число ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int m = int.Parse(Console.ReadLine());
+int max = n;
+if(m > max)
+    Console.Write($"max = {m}");
+else
+    Console.Write($"max = {n}");
