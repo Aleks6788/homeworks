4831233463655be72f1c2e4d9bf9d2b8d1750856
leshchev alekcei <luolikl@rambler.ru>
2023-02-16 15:40:23 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 4
+
+Console.Write("Введите 1 число ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите 2 число ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите  3 число ");
+int c = int.Parse(Console.ReadLine());
+int max = n;
+if(m > max) max = m;
+if(c > max) max = c;
+    Console.Write($"Максимальное число = {max}");    
