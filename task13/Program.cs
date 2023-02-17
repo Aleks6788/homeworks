3a4661a39b92e891e86bdf8e56bd15b564f6f4a3
leshchev alekcei <luolikl@rambler.ru>
2023-02-17 12:25:50 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 13
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine());
+if(n < 100)
+Console.Write("Третьей цифры нет");
+else
+{
+while(n > 1000)
+n = n / 10;
+Console.Write(n % 10);
+}
