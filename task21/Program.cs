11d05991e805e1693f209af3fd6a0c337871f0f5
leshchev alekcei <luolikl@rambler.ru>
2023-02-17 12:54:58 +0300

    task21

---
+++ task21/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 21
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int a = n % 10;
+int a1 = n / 10000;
+int b = n / 10 % 10;
+int b1 = n / 1000 % 10;
+if(a == a1 && b == b1)
+Console.Write($"Yes");
+else
+Console.Write($"No");
+
