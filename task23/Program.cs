ad8c129613c2e15fb824926ce31aa90bd23c0564
leshchev alekcei <luolikl@rambler.ru>
2023-02-23 14:02:51 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+void Numbers(int n)
+{
+    for(int i = 1; i <= n; i++)
+    Console.WriteLine(Math.Pow(i, 3));
+}
+
+Console.Clear();
+Console.Write("Введите число  ");
+int n = int.Parse(Console.ReadLine());
+Numbers( n);
+
+
