c3fcf5a6eecf8feaee580ea18510c2e44ef9cf6f
leshchev alekcei <luolikl@rambler.ru>
2023-02-23 10:53:56 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Создать массив, найти кол-во каждого элл. массива.
+void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11);    
+}
+void ReleasArray(int[] array) 
+{
+     for (int i = 0; i < array.Length; i++) 
+        {
+            int count = 0; 
+            for (int j = 0; j < array.Length; j++)
+            {
+
+                if (array[i] == array[j])
+                    count ++;
+            }
+            Console.WriteLine($"Эллимент {array[i]} встречается {count} раз");
+        }
+}
+
+Console.Clear();
+Console.Write("Введите колич. элл. массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: {string.Join(", ", array)}");
+ReleasArray(array);
