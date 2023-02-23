40ad5c9126736c58486db697ca2948c7f0c7a3df
leshchev alekcei <luolikl@rambler.ru>
2023-02-23 13:57:02 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19
+string palindrome(int n)
+{
+    int a = n % 10;
+    int a1 = n / 10000;
+    int b = n / 10 % 10;
+    int b1 = n / 1000 % 10;
+    if (a == a1 && b == b1)
+   return "Да";
+else
+return "Нет";
+}
+
+Console.Clear();
+Console.Write("Введите пятизначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+palindrome(n);
+Console.Write($"Введенное число палиндром ? - {palindrome(n)}");
