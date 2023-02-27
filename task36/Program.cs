62c5908ef24985ae48b2f64a6161cf6052dde510
leshchev alekcei <luolikl@rambler.ru>
2023-02-27 15:58:46 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36
+void InputArrat(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11);
+    Console.WriteLine($"Массив [{string.Join(", ", array)}])");
+}
+int Evenposition(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элл. ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+InputArrat(array);
+Console.Write($"Сумма элл. стоящих на нечет. порзициях = {Evenposition(array)}");
