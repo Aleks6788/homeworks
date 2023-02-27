e5e4f849677082d2d2b3abf560d44997331953e1
leshchev alekcei <luolikl@rambler.ru>
2023-02-27 13:38:21 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29
+void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 10);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элл. массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+
