7bab30cae4f9ae576d5ca85c234a1a8e535de42c
leshchev alekcei <luolikl@rambler.ru>
2023-02-27 15:27:19 +0300

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34
+void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+    Console.WriteLine($"Массив: [{string.Join(", ",array)}]");
+}
+void Evennumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+    if(array[i] % 2 == 0)
+    count ++;
+    }
+    Console.WriteLine($"Количество положительных элл. - {count}");
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во эллиментов ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Evennumbers(array);
