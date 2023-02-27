66ba145351e471ca3ee4a76e6797092c27707611
leshchev alekcei <luolikl@rambler.ru>
2023-02-27 13:01:33 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27
+int Sumnumbers(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine());
+Console.Write(Sumnumbers(n));
