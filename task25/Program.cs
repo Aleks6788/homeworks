b20504125266ee8e5d85cbdef8af03ab36a96a9d
leshchev alekcei <luolikl@rambler.ru>
2023-02-27 11:46:00 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25
+int Numbers(int a, int b)
+{
+    int result = 1;
+    int count = 1;
+    while (count <= b)
+    {
+        result = result * a;
+        count++;
+    }
+    return result;
+}
+
+
+Console.Write("Введите а -  ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b -  ");
+int b = int.Parse(Console.ReadLine());
+Console.Write($"a в нвтуральной степени b = {Numbers(a, b)}");
