c8025f2fbcb58e573e14a577ec1ef3d0a063a409
leshchev alekcei <luolikl@rambler.ru>
2023-02-28 16:09:10 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 41
+Console.Write("Введите числа ");
+string[] n = Console.ReadLine().Split(" ");
+int count = 0;
+foreach (string i in n) 
+{
+    if (int.Parse(i) > 0)
+        count++;
+}
+Console.Write($"Чисел больше нуля: {count}");
