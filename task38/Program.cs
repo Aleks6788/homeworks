bf4edb6dd6460c7e0254410687ebb0760c0ddf4b
leshchev alekcei <luolikl@rambler.ru>
2023-02-28 12:05:19 +0300

    task38

---
+++ task38/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 38
+void InputArray(double[] array)
+{
+
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rand.Next(1, 10) + rand.NextDouble();
+}
+double MinNumbers(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i])
+            min = array[i];
+    }
+    return min;
+}
+double MaxNumbers(double[] array)
+{
+    double max = array[0];
+     for (int i = 0; i < array.Length; i++)
+     {
+        if(max < array[i])
+        max = array[i];
+     }
+     return max;
+}
+
+
+Console.Clear();
+double[] array = new double[5];
+InputArray(array);
+Console.WriteLine($"Массив: {string.Join(", ", array)}");
+Console.WriteLine($"Наименьшее число {MinNumbers(array)}");
+Console.WriteLine($"Наибольшее число {MaxNumbers(array)}");
+Console.WriteLine($"Разница: {MaxNumbers(array) - MinNumbers(array)}");
