5002b9befb86428d440ab1d99fa402534cb81fa9
leshchev alekcei <luolikl@rambler.ru>
2023-02-28 16:22:41 +0300

    task42

---
+++ task42/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 42
+Console.Write("Введите координаты первой прямой ");
+string[] numbersFirst = Console.ReadLine().Split(" ");
+double k1 = Convert.ToDouble(numbersFirst[0]); 
+double b1 = Convert.ToDouble(numbersFirst[1]);
+Console.Write("Введите координаты второй прямой ");
+string[] numbersSecond = Console.ReadLine().Split(" ");
+double k2 = Convert.ToDouble(numbersSecond[0]);
+double b2 = Convert.ToDouble(numbersSecond[1]);
+Console.WriteLine((b2 - b1) / (k1 - k2) );
+Console.Write(k2 * (b2 - b1) / (k1 - k2) + b2);
+
