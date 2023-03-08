6774bb67a494c4a75ae622d9ad393903931a4a69
leshchev alekcei <luolikl@rambler.ru>
2023-03-08 11:47:51 +0300

    task50

---
+++ task50/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 50
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        { 
+         matrix[i, j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+            }
+        Console.WriteLine();
+    }
+}
+void ReleaseMatrix(int[,] matrix, int x, int y)
+{
+    if(x >= matrix.GetLength(0) || y >= matrix.GetLength(1) || x < 0 || y < 0)
+        Console.WriteLine("Таких координат нет ");
+    else Console.WriteLine(matrix[x, y]);
+    
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); 
+int[,] matrix = new int[size[0], size[1]]; 
+InputMatrix(matrix);
+Console.Write("Введите позицию (coord) элл. массива ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); 
+ReleaseMatrix( matrix, coord[0] - 1, coord[1] -1);
